ac5d0cf35e1127431832883f6529103a365c832a
mutinhiri <barnabmutinhiri@gmail.com>
2021-11-17 11:35:09 +0200

    Add changes and fix linter errors

--- src/dom.js
+++ src/dom.js
@@ -1,3 +1,3 @@
-const domElement = [{name: 'Barna', score: 20}, {name: "Svabi", score: 50}, {name: 'Joel', score: 30}]
+const domElement = [ { name: 'Barna', score: 20 }, { name: 'Svabi', score: 50 }, { name: 'Joel', score: 30 } ];
 
-export default domElement
+export default domElement;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import './style.css';
-import domElement from './dom';
+import domElement from './dom.js';
 
 const dom = document.querySelector('.data');
 
- const domItems = ()=>{
-   domElement.forEach((e)=>{
-     const li = document.createElement('li')
-     li.innerHTML = `${e.name}:${e.score}`;
-     dom.appendChild(li)
+const domItems = () => {
+  domElement.forEach((e) => {
+    const li = document.createElement('li');
+    li.innerHTML = `${e.name}:${e.score}`;
+    dom.appendChild(li)
+  });
+};
 
-   });
- };
-
-domItems()
+domItems();
