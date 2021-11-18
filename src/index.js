128175ab447ddcd37c5a45f7be13440935b7e3b6
mutinhiri <barnabmutinhiri@gmail.com>
2021-11-19 00:40:36 +0200

    Fix functionality error

--- src/index.js
+++ src/index.js
@@ -1,34 +1,30 @@
 import './style.css';
 import domItems from './dom.js';
-import { getScores, postScores } from "./api-Call";
+import { getScores, postScores } from './api-Call.js';
 
-const game_id = 'aJt95NEpxjlxmmqFfIJL';
+const GAME_ID = 'aJt95NEpxjlxmmqFfIJL';
 
-const refresh = document.getElementById('refresh')
+const refresh = document.getElementById('refresh');
 const form = document.getElementById('form');
 
 refresh.addEventListener('click', () => {
-  getScores(game_id).then((scores)=>{
-    domItems(scores.result)
+  getScores(GAME_ID).then((scores) => {
+    domItems(scores.result);
   });
-
 });
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const body = {
     user: document.getElementById('user').value,
-    score: parseInt(document.getElementById('score').value,10)
+    score: parseInt(document.getElementById('score').value, 10),
   };
 
   postScores(game_id, body);
   document.getElementById('user').value = '';
   document.getElementById('score').value = '';
- 
 });
 
 getScores(game_id).then((scores) => {
   domItems(scores.result)
 });
-
-// domItems();
