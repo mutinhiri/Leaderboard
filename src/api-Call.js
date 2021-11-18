2599f90053e4625f6c0b4912ad9e39f4850fcc2a
mutinhiri <barnabmutinhiri@gmail.com>
2021-11-18 17:19:22 +0200

    Add post functionality

--- src/api-Call.js
+++ src/api-Call.js
@@ -3,4 +3,16 @@ const getScores = async (id) => {
   const response = await fetch(url, {method:'GET'});
   const scores = await response.json();
   return scores;
-}
+};
+
+const postScores = async (id, data) =>{
+  await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  });
+};
+
+export {getScores, postScores}
