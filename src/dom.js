1f52b19eb975a14e38564cdff5e3e21a2a48f4cf
mutinhiri <barnabmutinhiri@gmail.com>
2021-11-18 18:50:24 +0200

    Add changes

--- src/dom.js
+++ src/dom.js
@@ -1,5 +1,7 @@
 // const domElement = [{ name: 'Barna', score: 20 }, { name: 'Svabi', score: 50 }, { name: 'Joel', score: 30 }];
 
+import { getScores, postScores } from "./api-Call";
+
 // export default domElement;
 
 const game_id = 'aJt95NEpxjlxmmqFfIJL';
@@ -8,5 +10,14 @@ const refresh = document.getElementById('refresh')
 const form = document.getElementById('form');
 
 refresh.addEventListener('submit', (e) =>{
-  e.preventDefault()
-})
+  e.preventDefault();
+  const body = {
+    user: document.getElementById('user'),
+    score: parseInt(document.getElementById('score').value,10)
+  };
+  postScores(game_id, body);
+  document.getElementById('user').value = '';
+  document.getElementById('score').value = '';
+});
+
+getScores(game_id).then((scores.result))
