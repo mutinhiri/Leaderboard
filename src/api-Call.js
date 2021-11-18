b717704f4ca1f29dd1898c3ce173ad6e5d2befcc
mutinhiri <barnabmutinhiri@gmail.com>
2021-11-19 00:31:09 +0200

    Add async wait functions

--- src/api-Call.js
+++ src/api-Call.js
@@ -1,20 +1,19 @@
-
 const getScores = async (id) => {
-  const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`
-  const response = await fetch(url, {method:'GET'});
+  const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`;
+  const response = await fetch(url, { method: 'GET' });
   const scores = await response.json();
   return scores;
 };
 
-const postScores = async (id, data) =>{
-  const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`
+const postScores = async (id, data) => {
+  const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`;
   await fetch(url, {
     method: 'post',
     body: JSON.stringify(data),
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
   });
 };
 
-export {getScores, postScores}
+export { getScores, postScores };
